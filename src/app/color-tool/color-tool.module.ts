3a7438612f99f81470724ca4862ea9d5a7d7fb5e
Eric W. Greene <eric@ericwgreene.com>
2018-02-12 14:56:07 -0500

    added code demos

--- src/app/color-tool/color-tool.module.ts
+++ src/app/color-tool/color-tool.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, ReactiveFormsModule,
   ],
   declarations: [ColorHomeComponent],
   // exports the component to be used in a template outside of the module
--- src/app/color-tool/components/color-home/color-home.component.ts
+++ src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { Color } from '../../models/color';
 
 @Component({
   selector: 'color-home',
@@ -9,15 +12,30 @@ export class ColorHomeComponent implements OnInit {
 
   public message = 'Color Tool';
 
-  public colors = [
+  public colorForm: FormGroup;
+
+  public colors: Color[] = [
     { id: 1, name: 'red' },
     { id: 2, name: 'blue' },
     { id: 3, name: 'green' },
   ];
 
-  constructor() { }
+  // private fb: FormBuilder;
+
+  // constructor(fb: FormBuilder) {
+  //   this.fb = fb;
+  // }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.colorForm = this.fb.group({
+      colorName: [ '' ],
+    });
+  }
+
+  addColor() {
+    console.log(this.colorForm.value);
   }
 
 }
