6186ea0d73a4eda419fd1789c9f5ce6a2acc3dd7
Eric W. Greene <eric@ericwgreene.com>
2018-02-13 09:19:00 -0500

    added code demos

--- src/app/color-tool/color-tool.module.ts
+++ src/app/color-tool/color-tool.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ColorHomeComponent } from './components/color-home/color-home.component';
+import { ToolHeaderComponent } from './components/tool-header/tool-header.component';
 
 @NgModule({
   imports: [
     CommonModule, ReactiveFormsModule,
   ],
-  declarations: [ColorHomeComponent],
+  declarations: [ColorHomeComponent, ToolHeaderComponent],
   // exports the component to be used in a template outside of the module
   exports: [ColorHomeComponent],
 })
