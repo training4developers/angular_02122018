4a72127f91bec2c5a3328a906069624d597e97fe
Eric W. Greene <eric@ericwgreene.com>
2018-02-13 15:27:19 -0500

    added code demos

--- src/app/car-tool/car-tool.module.ts
+++ src/app/car-tool/car-tool.module.ts
@@ -5,12 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CarHomeComponent } from './components/car-home/car-home.component';
 import { ToolHeaderComponent } from './components/tool-header/tool-header.component';
 import { TableRowComponent } from './components/table-row/table-row.component';
+import { CarTableComponent } from './components/car-table/car-table.component';
 
 @NgModule({
   imports: [
     CommonModule, ReactiveFormsModule,
   ],
-  declarations: [CarHomeComponent, ToolHeaderComponent, TableRowComponent],
+  declarations: [CarHomeComponent, ToolHeaderComponent, TableRowComponent, CarTableComponent],
   exports: [CarHomeComponent],
 })
 export class CarToolModule { }
---
+++ src/app/car-tool/components/car-table/car-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Car } from '../../models/car';
+
+@Component({
+  selector: 'car-table',
+  templateUrl: './car-table.component.html',
+  styleUrls: ['./car-table.component.css']
+})
+export class CarTableComponent implements OnInit {
+
+  @Input()
+  public cars: Car[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
